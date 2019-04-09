b5b7b0b923643e2ea12a013c3bce8918a57f6c46
Yongkun Gui <ygui@google.com>
2019-04-09 10:28:05 -0700

    Hack: Fix a cert issue in controller-runtime
    The error on API server:
    x509: cannot validate certificate for 100.116.232.47 because it doesn't contain any IP SANs
    
    TODO: fix upstream

--- vendor/sigs.k8s.io/controller-runtime/pkg/webhook/internal/cert/generator/selfsigned.go
+++ vendor/sigs.k8s.io/controller-runtime/pkg/webhook/internal/cert/generator/selfsigned.go
@@ -20,6 +20,7 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"fmt"
+	"net"
 	"time"
 
 	"k8s.io/client-go/util/cert"
@@ -72,9 +73,20 @@ func (cp *SelfSignedCertGenerator) Generate(commonName string) (*Artifacts, erro
 	if err != nil {
 		return nil, fmt.Errorf("failed to create the private key: %v", err)
 	}
+
+	var altNames cert.AltNames
+	ip := net.ParseIP(commonName)
+	fmt.Printf("alt names: %v", ip)
+	if ip != nil {
+		altNames.IPs = []net.IP{ip}
+	} else {
+		altNames.DNSNames = []string{commonName}
+	}
+
 	signedCert, err := cert.NewSignedCert(
 		cert.Config{
 			CommonName: commonName,
+			AltNames:   altNames,
 			Usages:     []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
 		},
 		key, signingCert, signingKey,
