43e8d57c966bdf74d323f6a4eceecef64e66441c
Anfernee Yongkun Gui <ygui@google.com>
2019-04-08 23:52:08 -0700

    Add webhook
    controller-runtime's webhook library generete invalid cert. It needs to
    contain valid SAN.

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -20,19 +20,28 @@ import (
 	"os"
 
 	"github.com/anfernee/k8s-ipam-webhook/pkg/apis"
-	"github.com/anfernee/k8s-ipam-webhook/pkg/controller"
-	"github.com/anfernee/k8s-ipam-webhook/pkg/webhook"
+	ipamv1beta1 "github.com/anfernee/k8s-ipam-webhook/pkg/apis/ipam/v1beta1"
+	"github.com/anfernee/k8s-ipam-webhook/pkg/webhook/ipam"
+	admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1beta1"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"sig.k8s.io/cluster-api/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/signals"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission/builder"
 )
 
 func main() {
+	disableWebhookConfigInstaller := false
+
 	var metricsAddr string
+	var host string
 	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&host, "host", "localhost", "IP address to listen to")
 	flag.Parse()
+
 	logf.SetLogger(logf.ZapLogger(false))
 	log := logf.Log.WithName("entrypoint")
 
@@ -68,9 +77,38 @@ func main() {
 		os.Exit(1)
 	}
 
+	// register webhook
 	log.Info("setting up webhooks")
-	if err := webhook.AddToManager(mgr); err != nil {
-		log.Error(err, "unable to register webhooks to the manager")
+	mutatingWebhook, err := builder.NewWebhookBuilder().
+		Name("mutating.k8s.io").
+		Mutating().
+		Operations(admissionregistrationv1beta1.Create, admissionregistrationv1beta1.Update).
+		WithManager(mgr).
+		ForType(&ipamv1beta1.Machine{}).
+		Handlers(&ipam.IPAMAllocator{}).
+		Build()
+	if err != nil {
+		log.Error(err, "unable to setup mutating webhook")
+		os.Exit(1)
+	}
+
+	log.Info("setting up webhook server")
+	as, err := webhook.NewServer("webhook-admission-server", mgr, webhook.ServerOptions{
+		BootstrapOptions: &webhook.BootstrapOptions{
+			Host: &host,
+		},
+		Port:                          9876,
+		CertDir:                       "/tmp/cert",
+		DisableWebhookConfigInstaller: &disableWebhookConfigInstaller,
+	})
+	if err != nil {
+		log.Error(err, "unable to create a new webhook server")
+		os.Exit(1)
+	}
+
+	err = as.Register(mutatingWebhook)
+	if err != nil {
+		log.Error(err, "unable to register webhooks in the admission server")
 		os.Exit(1)
 	}
 
---
+++ pkg/webhook/ipam/ipam_webhook.go
@@ -0,0 +1,85 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ipam
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	ipamv1beta1 "github.com/anfernee/k8s-ipam-webhook/pkg/apis/ipam/v1beta1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/runtime/inject"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission/types"
+)
+
+// IPAMAllocator annotates Pods
+type IPAMAllocator struct {
+	client  client.Client
+	decoder types.Decoder
+}
+
+// FIXME: Not used
+func Add(mgr manager.Manager) error {
+	return nil
+}
+
+// Implement admission.Handler so the controller can handle admission request.
+var _ admission.Handler = &IPAMAllocator{}
+
+// IPAMAllocator adds an annotation to every incoming machine.
+func (a *IPAMAllocator) Handle(ctx context.Context, req types.Request) types.Response {
+	log := logf.Log.WithName("entrypoint")
+
+	log.Info("receive webhook request")
+	fmt.Println("receive webhook request")
+
+	machine := &ipamv1beta1.Machine{}
+
+	err := a.decoder.Decode(req, machine)
+	if err != nil {
+		return admission.ErrorResponse(http.StatusBadRequest, err)
+	}
+	copy := machine.DeepCopy()
+
+	if copy.Annotations == nil {
+		copy.Annotations = map[string]string{}
+	}
+	copy.Annotations["example-mutating-admission-webhook"] = "foo"
+
+	return admission.PatchResponse(machine, copy)
+}
+
+// IPAMAllocator implements inject.Client.
+// A client will be automatically injected.
+var _ inject.Client = &IPAMAllocator{}
+
+// InjectClient injects the client.
+func (v *IPAMAllocator) InjectClient(c client.Client) error {
+	v.client = c
+	return nil
+}
+
+// IPAMAllocator implements inject.Decoder.
+// A decoder will be automatically injected.
+var _ inject.Decoder = &IPAMAllocator{}
+
+// InjectDecoder injects the decoder.
+func (v *IPAMAllocator) InjectDecoder(d types.Decoder) error {
+	v.decoder = d
+	return nil
+}
